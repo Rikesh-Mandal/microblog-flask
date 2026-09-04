f5669a0cf119860df3ea1b0e59de8d29c94fb2d8
Rikesh Mandal <rikesh.mandal26@gmail.com>
2026-09-04 22:01:35 +0100

    Made Elasticsearch Indexing fault tolerant

--- app/search.py
+++ app/search.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from elasticsearch.exceptions import ConnectionError
 
 def add_to_index(index,model):
     if not current_app.elasticsearch:
@@ -6,24 +7,49 @@ def add_to_index(index,model):
     payload={}
     for field in model.__searchable__:
         payload[field]=getattr(model, field)
-    current_app.elasticsearch.index(index=index, id=model.id, document=payload)
+    try:
+        current_app.elasticsearch.index(index=index, id=model.id, document=payload)
+    except ConnectionError:
+        current_app.logger.warning(
+            'Elasticsearch is unavailable. '
+            'Skipping indexing for %s id=%s',
+            index,
+            model.id
+        )
+
 
 
 def remove_from_index(index,model):
     if not current_app.elasticsearch:
         return
-    current_app.elasticsearch.delete(index=index, id=model.id)
+    try:
+        current_app.elasticsearch.delete(index=index, id=model.id)
+    except ConnectionError:
+        current_app.logger.warning(
+            'Elasticsearch is unavailable. '
+            'Skipping indexing for %s id=%s',
+            index,
+            model.id
+        )
+
 
 
 def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
-    search = current_app.elasticsearch.search(
-        index=index,
-        query={'multi_match': {'query': query, 'fields': ['*']}},
-        from_=(page - 1) * per_page,
-        size=per_page
-    )
+    try:
+        search = current_app.elasticsearch.search(
+            index=index,
+            query={'multi_match': {'query': query, 'fields': ['*']}},
+            from_=(page - 1) * per_page,
+            size=per_page
+        )
+    except ConnectionError:
+        current_app.logger.warning(
+            'Elasticsearch is unavailable. '
+            'Search request could not be completed.'
+        )
+        return [], 0
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
     
